0133d4de100ed4dad74e74caef4cfc33f74222ee
Javier Donadio <javier.donadio@globant.com>
2019-03-11 17:32:02 -0300

    update user image properly

--- src/app/user/user.page.ts
+++ src/app/user/user.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, NgZone } from '@angular/core';
 import { SharingService } from '../services/sharing.service';
 import { AngularFireDatabase } from 'angularfire2/database';
-import * as firebase from 'firebase/app';
+import { LoadingController, ToastController } from '@ionic/angular';
+import * as firebase from 'firebase';
 import * as _ from 'lodash';
 
 @Component({
@@ -14,22 +15,28 @@ export class UserPage implements OnInit {
   public user: any;
   public rankingUrl: string;
   public record: any;
-  public photo: any;
   public photoURL: any;
+  public imageLoaded: boolean;
+  private loading: any;
 
   constructor(
     private sharingService: SharingService,
     private zone: NgZone,
     private db: AngularFireDatabase,
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController
   ) {
     this.photoURL = null;
+    this.imageLoaded = false;
     this.sharingService.currentUser.subscribe(user => {
+      console.log(user);
       if (user.history) user.history = _.orderBy(user.history, ['updatedAt'], ['desc']);
+
+      this.rankingUrl = `assets/icon/rank-${user.category.value}.jpg`;
+      this.record = user.history ? _.countBy(user.history, 'winners') : null;
+
       this.zone.run(() => {
         this.user = _.clone(user);
-        this.rankingUrl = `assets/icon/rank-${user.category.value}.jpg`;
-        this.record = user.history ? _.countBy(user.history, 'winners') : null;
-        console.log(user);
       });
     });
   }
@@ -37,6 +44,7 @@ export class UserPage implements OnInit {
   ngOnInit() {}
 
   loadPhoto(event) {
+    this.user.imgURL = null;
     this.readThis(event.target);
   }
 
@@ -45,29 +53,48 @@ export class UserPage implements OnInit {
     var myReader:FileReader = new FileReader();
   
     myReader.onloadend = (e) => {
-      this.photo = myReader.result;
-      this.photoURL = this.photo;
-      // this.photoURL = 'data:image/jpeg;base64,' + this.photo;
+      this.photoURL = myReader.result;
+      this.imageLoaded = true;
     }
     myReader.readAsDataURL(file);
   }
 
-  uploadPhoto() {
+  async uploadPhoto() {
     let storageRef = firebase.storage().ref();
-    const filename = this.user.alias;
-    const imageRef = storageRef.child(`users/${filename}.jpg`);
+    const filename = Math.random().toString(36).substring(7);
+    const imageRef = storageRef.child(`users/${this.user.alias}/${filename}.jpg`);
 
-    imageRef.putString(this.photoURL, firebase.storage.StringFormat.DATA_URL).then((snapshot) => {
-      this.updateUser(snapshot.downloadURL);
-    });
+    this.loading = await this.loadingCtrl.create({ message: 'Actualizando imagen...' });
+    this.loading.present();
+
+    try {
+      imageRef.putString(this.photoURL, 'data_url')
+        .then((snapshot) => { 
+          snapshot.ref.getDownloadURL()
+          .then(url => { this.updateUser(url) });
+      });
+    } catch (error) {
+      this.propagateError(error);
+    }
   }
 
   private updateUser(url) {
     try {
       this.db.object('users/' + this.user.key).update({ imgURL: url });
-      this.photoURL = null;
+      this.loading.dismiss();
+      this.imageLoaded = false;
     } catch(err) {
-      console.log(err);
+      this.propagateError(err);
     }
+  }
+
+  async propagateError(error) {
+    console.log(error);
+    this.loading.dismiss();
+    const toast = await this.toastCtrl.create({
+      message: error,
+      duration: 4000
+    });
+    toast.present();
   }
 }
